11c866017832b137795dde8855ba366e717236f2
Mordi Sacks <mordisacks@gmail.com>
2021-02-12 12:58:00 +0200

    initial

---
+++ src/LaravelStarterServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MordiSacks\LaravelStarter;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelStarterServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
